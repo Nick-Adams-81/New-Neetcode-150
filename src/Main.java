06902652b1465237a985785bb9303a83e7323f91
Nicholas Adams <nicholasadams@Owners-MBP.attlocal.net>
2023-11-27 11:13:39 -0800

    added arrays package and contains duplicate

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import Arrays.ContainsDuplicate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        ContainsDuplicate duplicate = new ContainsDuplicate();
+        int[] dupes = {2, 3, 5, 2, 1};
+        System.out.println(duplicate.duplicates(dupes));
     }
 }
